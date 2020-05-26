1d364cf78c2545e2fffd1b0c015424a600af484e
nolandseigler <57370691+nolandseigler@users.noreply.github.com>
2020-05-26 13:56:57 -0500

    Integer exploration

--- IntegersExploration/IntegersExploration.cpp
+++ IntegersExploration/IntegersExploration.cpp
@@ -5,12 +5,15 @@
 extern "C" char GlChar = 10;
 extern "C" short GlShort = 20;
 extern "C" int GlInt = 30;
-extern "C" long long GLongLong = 0x000000000FFFFFFFFF;
+extern "C" long long GLongLong = 344566;
 
 extern "C" void IntegerAddition(char a, short b, int c, long long d);
 int main()
 {
     IntegerAddition(3, 5, -37, 11);
 
+    printf("Char value %d\n", GlChar);
+    printf("Short value %d\n", GlShort);
     printf("Int value %d\n", GlInt);
+    printf("Long long value %11d\n", GLongLong);
 }
